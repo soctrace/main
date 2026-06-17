bcbd253ef42ac2db0ef721ea82dac1bb50b5f1ce
Jose Manuel Guzmán <josemanuelguzman@Mac-mini-de-Jose.local>
2026-06-17 10:19:03 +0200

    Tomas Gasset demo user

--- soctrace-web/src/auth/accessControl.ts
+++ soctrace-web/src/auth/accessControl.ts
@@ -26,6 +26,7 @@ export const FRIENDS_AND_FAMILY_ALLOWED_EMAILS = [
   "antoniotorroles81@gmail.com",
   "ricardocubiles@gmail.com",
   "trinidadbornao@gmail.com",
+  "tgp@byurbania.com",
 ] as const;
 
 export const FRIENDS_AND_FAMILY_ALLOWED_EMAIL_SET = new Set<string>(
